52959f66ac8de3b06142996395173edf4751a48d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-06-26 22:09:32 -0600

    feat: :sparkles: Auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IResponse } from '../interfaces/response.interface';
+import { IResponseLogin } from '../interfaces/IResponseLogin.interface';
+import { IResponseAuthLog } from '../interfaces/IResponseAuthLog.interface';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -11,9 +12,21 @@ export class AuthService {
   private hostApiSpox = environment.hostApiSpox;
   constructor(private http: HttpClient) {}
 
-  login(): Observable<IResponse> {
-    return this.http.get<IResponse>(`${this.hostApiSpox}api/v1/auth/login`, {
-      withCredentials: true,
-    });
+  login(): Observable<IResponseLogin> {
+    return this.http.get<IResponseLogin>(
+      `${this.hostApiSpox}api/v1/auth/login`,
+      {
+        withCredentials: true,
+      }
+    );
+  }
+
+  getAuthLog(id: string | null): Observable<IResponseAuthLog> {
+    return this.http.get<IResponseAuthLog>(
+      `${this.hostApiSpox}api/v1/auth/get-log-userid?userid=${id}`,
+      {
+        withCredentials: true,
+      }
+    );
   }
 }
