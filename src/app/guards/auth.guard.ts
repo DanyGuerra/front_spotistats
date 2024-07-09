57cb1fa41e6e0d68f6b267061d5746ea3fe86fff
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-07-08 21:11:17 -0600

    feat: :sparkles: Set items LocalStorage in AuthGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,6 +2,7 @@ import { CanActivateFn, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { firstValueFrom } from 'rxjs';
+import { LocalStorage } from 'src/constants/localStorage';
 
 export const authGuard: CanActivateFn = async (
   route: ActivatedRouteSnapshot
@@ -15,8 +16,11 @@ export const authGuard: CanActivateFn = async (
     const response = await firstValueFrom(authService.getAuthLogByUserId(id));
 
     if (response.statusCode === 200) {
+      localStorage.setItem(LocalStorage.LogId, response.data._id);
+      localStorage.setItem(LocalStorage.UserId, response.data.usernameId);
       return true;
     } else {
+      localStorage.clear();
       router.navigate(['/']);
       return false;
     }
