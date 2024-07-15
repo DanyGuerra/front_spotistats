cb88e97cae3ebd194ea824384daaa846cac183cf
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-07-15 16:46:38 -0600

    refactor: :recycle: Remove resolver

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IResponseAuthLog } from 'src/app/interfaces/IResponseAuthLog.interface';
-
-import { AuthService } from 'src/app/services/auth.service';
-import { LocalStorage } from 'src/constants/localStorage';
+import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { CardModule } from 'primeng/card';
 import { ImageModule } from 'primeng/image';
@@ -25,18 +20,4 @@ import { TabsStatsComponent } from '../tabs-stats/tabs-stats.component';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.less'],
 })
-export class UserprofileComponent implements OnInit {
-  constructor(
-    private route: ActivatedRoute,
-    private authService: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    this.route.data.subscribe(({ authData }) => {
-      const infoResponse: IResponseAuthLog = authData;
-      localStorage.setItem(LocalStorage.LogId, infoResponse.data._id);
-      localStorage.setItem(LocalStorage.UserId, infoResponse.data.usernameId);
-      this.authService.setAuthenticated(true);
-    });
-  }
-}
+export class UserprofileComponent {}
