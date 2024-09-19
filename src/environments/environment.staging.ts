3819cb56da1a230c35a0235b1975fb1da04c63bb
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-09-19 11:57:45 -0600

    feat: host api

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  hostApiSpox: 'https://spotistatistics-fd33d8058a18.herokuapp.com/',
+  hostApiSpox: 'https://spotistats-yz3q.onrender.com/',
   hostApiSpoxContext: 'api/v1/',
 };
