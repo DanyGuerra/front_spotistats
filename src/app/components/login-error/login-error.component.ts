0f8d2ed6abe61cb195006cd96fc503f8c940c2ab
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-12-07 17:10:42 -0600

    feat: :sparkles: Login error page

--- src/app/components/login-error/login-error.component.ts
+++ src/app/components/login-error/login-error.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-login-error',
   templateUrl: './login-error.component.html',
-  styleUrls: ['./login-error.component.less']
+  styleUrls: ['./login-error.component.less'],
 })
 export class LoginErrorComponent {
-
+  constructor(
+    private authService: AuthService,
+    private toastService: ToastService
+  ) {}
+  login() {
+    this.authService.login().subscribe({
+      next: (response) => {
+        const {
+          data: { url },
+        } = response;
+        window.location.href = url;
+      },
+      error: () => {
+        this.toastService.showError('Error', 'Something went wrong, try later');
+      },
+    });
+  }
 }
