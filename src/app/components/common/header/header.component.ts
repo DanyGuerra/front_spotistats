3d8a5a09fd4dfd0ae45c0863e38424370aab78d8
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-12-06 23:29:57 -0600

    feat: :sparkles: Header responsive with user funtions

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -7,17 +7,32 @@ import { LocalStorage } from 'src/constants/localStorage';
 import { Subscription } from 'rxjs';
 import { IconlogoComponent } from '../icons/iconlogo/iconlogo.component';
 import { ToastService } from 'src/app/services/toast.service';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { IUserInfoStored } from 'src/app/interfaces/IUserInfoStored';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, ButtonModule, RouterModule, IconlogoComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    RouterModule,
+    IconlogoComponent,
+    MenuModule,
+    AvatarModule,
+    RippleModule,
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   private authSuscription: Subscription | null = null;
   isAuthenticated: boolean = false;
+  items: MenuItem[] | undefined;
+  userData!: IUserInfoStored | null;
 
   constructor(
     private authService: AuthService,
@@ -26,19 +41,59 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    const idLog = localStorage.getItem(LocalStorage.LogId);
+    const storedUser = localStorage.getItem(LocalStorage.UserInfo);
+    const userInfo: IUserInfoStored | null = JSON.parse(storedUser!!);
 
-    if (idLog) {
-      this.authService.getAuthLog(idLog).subscribe({
-        next: () => {
-          this.authService.setAuthenticated(true);
-        },
-        error: () => {
-          this.authService.setAuthenticated(false);
-          localStorage.removeItem(LocalStorage.LogId);
-        },
-      });
+    this.userData = userInfo;
+
+    this.items = [
+      {
+        label: 'My stats',
+        items: [
+          {
+            label: 'Top artists',
+            icon: 'pi pi-users',
+            command: () => this.navigateTo(`${userInfo?.userId}/top-artists`),
+          },
+          {
+            label: 'Top tracks',
+            icon: 'pi pi-play-circle',
+            command: () => this.navigateTo(`${userInfo?.userId}/top-tracks`),
+          },
+          {
+            label: 'Recently played',
+            icon: 'pi pi-headphones',
+            command: () =>
+              this.navigateTo(`${userInfo?.userId}/recently-played`),
+          },
+        ],
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'My profile',
+            icon: 'pi pi-user',
+            command: () => this.navigateTo(`${userInfo?.userId}`),
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: () => this.logout(),
+          },
+        ],
+      },
+      {
+        separator: true,
+      },
+    ];
+
+    if (userInfo) {
+      this.authService.setAuthenticated(true);
+    } else {
+      this.authService.setAuthenticated(false);
     }
+
     this.authSuscription = this.authService
       .isAuthenticated()
       .subscribe((isAuthenticated) => (this.isAuthenticated = isAuthenticated));
@@ -63,15 +118,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   logout() {
-    this.authService.logout();
+    const storedUser = localStorage.getItem(LocalStorage.UserInfo);
+    const userInfo: IUserInfoStored | null = JSON.parse(storedUser!!);
+    const logId = userInfo?.logId;
+
+    this.authService.logout(logId);
   }
 
-  goMyProfile() {
-    const displayName = localStorage.getItem(LocalStorage.UserId);
-    if (displayName) {
-      this.router.navigate([`/${displayName}`]);
-    } else {
-      this.router.navigate(['/']);
-    }
+  navigateTo(url: string) {
+    this.router.navigate([url]);
+  }
+
+  navigateToExternalUrl(url: string): void {
+    this.router.navigateByUrl(url, { skipLocationChange: true });
   }
 }
