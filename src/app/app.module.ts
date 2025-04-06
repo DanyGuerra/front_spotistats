86822527c2f541a139d6a0d910a0997f1647758d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-04-05 18:17:14 -0600

    refactor: version 18 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  HTTP_INTERCEPTORS,
-  HttpClient,
-  HttpClientModule,
-} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { PagenotfoundComponent } from './components/common/pagenotfound/pagenotfound.component';
 import { LoginErrorComponent } from './components/login-error/login-error.component';
 import { ButtonModule } from 'primeng/button';
@@ -24,33 +20,27 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PagenotfoundComponent,
-    LoginErrorComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HeaderComponent,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    ButtonModule,
-    ToastModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    MessageService,
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        PagenotfoundComponent,
+        LoginErrorComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        HeaderComponent,
+        BrowserAnimationsModule,
+        ButtonModule,
+        ToastModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        })], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        MessageService,
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-  HttpErrorResponse,
-} from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
