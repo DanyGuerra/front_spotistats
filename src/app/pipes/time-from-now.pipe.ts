2da63d4614c47c8fc13470e6abaa0f7603bff89e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-04-07 19:12:11 -0600

    feat: :sparkles: Pipe time from now listening language changes

--- src/app/pipes/time-from-now.pipe.ts
+++ src/app/pipes/time-from-now.pipe.ts
@@ -1,14 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
-import { DefaultLanguage, Language } from 'src/constants/types';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Pipe({
   name: 'timeFromNow',
   standalone: true,
+  pure: false,
 })
 export class TimeFromNowPipe implements PipeTransform {
-  transform(date: string, language: Language = DefaultLanguage): string {
-    moment.locale(language);
-    return moment.utc(date).fromNow();
+  private currentLang: string;
+  private lastValue!: string;
+  private lastResult!: string | null;
+  private subscription: Subscription;
+
+  constructor(private translate: TranslateService) {
+    this.currentLang =
+      this.translate.currentLang || this.translate.getDefaultLang();
+    moment.locale(this.currentLang);
+
+    this.subscription = this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang;
+      moment.locale(this.currentLang);
+      this.lastResult = null;
+    });
+  }
+
+  transform(value: string): string {
+    if (value !== this.lastValue || !this.lastResult) {
+      this.lastValue = value;
+      this.lastResult = moment.utc(value).fromNow();
+    }
+    return this.lastResult;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
