bbdc858cf796dbce640ba8ce1a7f614f8aacf446
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-05-16 19:54:31 -0600

    style: :lipstick: Logo animations completed

--- src/app/components/common/icons/iconlogo/iconlogo.component.ts
+++ src/app/components/common/icons/iconlogo/iconlogo.component.ts
@@ -28,12 +28,15 @@ export class IconlogoComponent implements AfterViewInit {
       thirdArrow: svg?.querySelector<SVGPathElement>('#thirdArrow')!,
       oSecondLetter: svg?.querySelector<SVGPathElement>('#oSecondLetter')!,
       aSixthLetter: svg?.querySelector<SVGPathElement>('#aSixthLetter')!,
-      asterik: svg?.querySelector<SVGPathElement>('#asterik')!,
+      asterisk: svg?.querySelector<SVGPathElement>('#asterisk')!,
       heart: svg?.querySelector<SVGPathElement>('#heart')!,
-      arrowsTogether: svg?.querySelector<SVGPathElement>('#arrows')!,
+      arrowsAll: svg?.querySelector<SVGPathElement>('#arrows')!,
+      musicWavesAll: svg?.querySelector<SVGPathElement>('#musicWaves')!,
       letters: svg?.querySelectorAll<SVGPathElement>('#words path') ?? [],
       tLetters: svg?.querySelectorAll<SVGPathElement>('.tLetter') ?? [],
       arrows: svg?.querySelectorAll<SVGPathElement>('.arrow') ?? [],
+      musicWaves:
+        svg?.querySelectorAll<SVGPathElement>('#musicWaves rect') ?? [],
     };
   }
 
@@ -44,6 +47,7 @@ export class IconlogoComponent implements AfterViewInit {
 
   onHover() {
     if (this.isAnimated()) return;
+
     this.isAnimated.set(true);
 
     const {
@@ -58,7 +62,7 @@ export class IconlogoComponent implements AfterViewInit {
       tLetters,
       arrows,
       aSixthLetter,
-      asterik,
+      asterisk,
     } = this.svgElements;
 
     const timeline = gsap.timeline({
@@ -71,7 +75,11 @@ export class IconlogoComponent implements AfterViewInit {
       () => this.verticalAnimation(timeline, thirdLetter, thirdArrow),
       () => this.beatAnimation(timeline, oSecondLetter, heart),
       () => this.verticalAnimation(timeline, tLetters, arrows),
-      () => this.spinAnimation(timeline, aSixthLetter, asterik),
+      () => this.spinAnimation(timeline, aSixthLetter, asterisk),
+      () => {
+        this.musicWavesAnimation(timeline);
+        this.beatAnimation(timeline, oSecondLetter, heart);
+      },
     ];
 
     const randomIndex = Math.floor(Math.random() * animations.length);
@@ -80,12 +88,12 @@ export class IconlogoComponent implements AfterViewInit {
   }
 
   private prepareInitialSvg() {
-    const { letters, arrows, arrowsTogether, heart } = this.svgElements;
+    const { arrows, arrowsAll, musicWavesAll, musicWaves } = this.svgElements;
 
-    gsap.set(letters, { y: -50 });
     gsap.set(arrows, { y: 50 });
-    gsap.set(arrowsTogether, { autoAlpha: 1 });
-    gsap.set(heart, { autoAlpha: 1, scale: 0, transformOrigin: '50% 50%' });
+    gsap.set(arrowsAll, { autoAlpha: 1 });
+    gsap.set(musicWavesAll, { autoAlpha: 1 });
+    gsap.set(musicWaves, { autoAlpha: 0 });
   }
 
   private initalAnimation() {
@@ -99,22 +107,10 @@ export class IconlogoComponent implements AfterViewInit {
 
     const { letters, oSecondLetter, heart } = this.svgElements;
 
-    this.bounceAnimation(timeline, letters);
+    timeline.set(letters, { y: 0 });
+    timeline.add(() => {}, 1);
+    this.musicWavesAnimation(timeline);
     this.beatAnimation(timeline, oSecondLetter, heart);
-  }
-
-  private bounceAnimation(timeline: gsap.core.Timeline, elements: NodeList) {
-    elements.forEach((element, index) => {
-      timeline.to(
-        element,
-        {
-          y: 0,
-          duration: 1,
-          ease: 'bounce.out',
-        },
-        index * 0.05
-      );
-    });
   }
 
   private verticalAnimation(
@@ -122,27 +118,10 @@ export class IconlogoComponent implements AfterViewInit {
     initialElement: SVGPathElement | NodeList,
     finalElement: SVGPathElement | NodeList
   ) {
-    timeline
-      .to(initialElement, {
-        duration: 0.25,
-        y: -50,
-        ease: 'elastic.in',
-      })
-      .to(finalElement, {
-        duration: 1,
-        y: 0,
-        ease: 'elastic.out',
-      })
-      .to(finalElement, {
-        duration: 1,
-        y: 50,
-        ease: 'elastic.in',
-      })
-      .to(initialElement, {
-        duration: 0.25,
-        y: 0,
-        ease: 'elastic.out',
-      });
+    this.fadeOut(timeline, initialElement);
+    this.fadeIn(timeline, finalElement);
+    this.fadeOut(timeline, finalElement);
+    this.fadeIn(timeline, initialElement);
   }
 
   private beatAnimation(
@@ -217,7 +196,7 @@ export class IconlogoComponent implements AfterViewInit {
         { autoAlpha: 0, transformOrigin: '50% 50%' },
         {
           rotate: 360,
-          duration: 2,
+          duration: 1,
           autoAlpha: 1,
           scale: 1.2,
           ease: 'elastic.out',
@@ -225,17 +204,88 @@ export class IconlogoComponent implements AfterViewInit {
       )
       .to(finalElement, {
         rotate: -360,
-        duration: 2,
+        duration: 1,
         scale: 0,
         autoAlpha: 0,
         ease: 'elastic.in',
       })
       .to(initialElement, {
         rotate: -360,
-        duration: 2,
+        duration: 1,
         autoAlpha: 1,
         scale: 1,
         ease: 'elastic.out',
       });
+  }
+
+  private fadeOut(
+    timeline: gsap.core.Timeline,
+    element: SVGPathElement | NodeList
+  ) {
+    timeline.fromTo(
+      element,
+      {
+        autoAlpha: 1,
+      },
+      {
+        y: 50,
+        duration: 1,
+        ease: 'elastic.in',
+        stagger: {
+          each: 0.025,
+        },
+      }
+    );
+  }
+
+  private fadeIn(
+    timeline: gsap.core.Timeline,
+    element: SVGPathElement | NodeList
+  ) {
+    timeline.to(element, {
+      duration: 1,
+      y: 0,
+      ease: 'elastic.out',
+      stagger: {
+        each: 0.025,
+      },
+    });
+  }
+
+  private musicWaveAnimation(timeline: gsap.core.Timeline, elements: NodeList) {
+    timeline
+      .fromTo(
+        elements,
+        {
+          scaleY: 0,
+          autoAlpha: 1,
+          transformOrigin: 'bottom',
+        },
+        {
+          scaleY: () => gsap.utils.random(0.15, 3),
+          repeat: 3,
+          yoyo: true,
+          duration: 0.25,
+          stagger: {
+            each: 0.1,
+            repeat: 3,
+            yoyo: true,
+          },
+          ease: 'sine.inOut',
+        }
+      )
+      .to(elements, {
+        scaleY: 0,
+        autoAlpha: 0,
+      });
+  }
+
+  private musicWavesAnimation(timeline: gsap.core.Timeline) {
+    const { letters, musicWaves } = this.svgElements;
+    this.isAnimated.set(true);
+
+    this.fadeOut(timeline, letters);
+    this.musicWaveAnimation(timeline, musicWaves);
+    this.fadeIn(timeline, letters);
   }
 }
