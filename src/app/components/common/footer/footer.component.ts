7e54de0a2cd0b1574ff5b86eac57abb1916a71cf
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-06-11 18:11:28 -0600

    feat: :sparkles: Translate footer and header timeline section

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DropdownLanguageComponent } from '../dropdown-language/dropdown-language.component';
 import { getYear } from 'date-fns';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [DropdownLanguageComponent],
+  imports: [DropdownLanguageComponent, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.less',
 })
--- src/app/components/common/icons/timeline-svg/timeline-svg.component.ts
+++ src/app/components/common/icons/timeline-svg/timeline-svg.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-timeline-svg',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './timeline-svg.component.html',
-  styleUrl: './timeline-svg.component.less'
+  styleUrl: './timeline-svg.component.less',
 })
-export class TimelineSvgComponent {
-
-}
+export class TimelineSvgComponent {}
