3433beabd04da8206864beb5a65452ca748840ff
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-07-04 17:14:57 -0600

    feat: :sparkles: host api test

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  hostApiSpox: 'https://zeihj32h63.execute-api.us-east-1.amazonaws.com/dev/',
+  hostApiSpox: 'https://s8rxoqvb35.execute-api.us-east-1.amazonaws.com/dev/',
   hostApiSpoxContext: 'api/v1/',
   xApiKey: '73My5wSVsg6brtvavvoNynmuFhbhIRl92q4nK1lg',
 };
