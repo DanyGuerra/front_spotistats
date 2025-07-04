d79bf91bda80437d6fb37274db7a8499d7170ead
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-07-03 19:46:55 -0600

    fix: :bug: Host api variable fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  hostApiSpox: 'https://zeihj32h63.execute-api.us-east-1.amazonaws.com/dev',
+  hostApiSpox: 'https://zeihj32h63.execute-api.us-east-1.amazonaws.com/dev/',
   hostApiSpoxContext: 'api/v1/',
 };
