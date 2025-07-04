eb96136b43c0a5f4d6648bf6a7a0019f14b8e5e3
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-07-04 17:39:38 -0600

    feat: :sparkles: Api host variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  hostApiSpox: 'https://s8rxoqvb35.execute-api.us-east-1.amazonaws.com/dev/',
+  hostApiSpox: 'https://v86mvg7ds2.execute-api.us-east-1.amazonaws.com/dev/',
   hostApiSpoxContext: 'api/v1/',
   xApiKey: '73My5wSVsg6brtvavvoNynmuFhbhIRl92q4nK1lg',
 };
