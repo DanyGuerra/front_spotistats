3a9e263f0075bd62109dcba07560fe24c01f993a
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-07-06 17:25:17 -0600

    feat: :sparkles: Enviroments configuration

--- src/environments/environment.staging.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  hostApiSpox: 'https://v86mvg7ds2.execute-api.us-east-1.amazonaws.com/dev/',
-  hostApiSpoxContext: 'api/v1/',
-  xApiKey: '73My5wSVsg6brtvavvoNynmuFhbhIRl92q4nK1lg',
-};
